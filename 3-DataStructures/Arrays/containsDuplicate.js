75069110895c1a6a4e14034c08d3015437f29255
Can Polat <can.polatt@yahoo.com>
2022-07-02 11:43:30 +0300

    Array solutions added

---
+++ 3-DataStructures/Arrays/containsDuplicate.js
@@ -0,0 +1,16 @@
+// Given an integer array nums, return true if any value appears at least
+// twice in the array, and return false if every element is distinct.
+
+//  @param {number[]} nums
+//  @return {boolean}
+
+var containsDuplicate = function (nums) {
+  const set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+    set.add(nums[i]);
+  }
+  return false;
+};
---
+++ 3-DataStructures/Arrays/maximumSubarray.js
@@ -0,0 +1,35 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the 
+largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+// Output: 23
+*/
+
+var maxSubArray = function (nums) {
+  let max = nums[0],
+    curr = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    curr = Math.max(nums[i], curr + nums[i]);
+    max = Math.max(max, curr);
+  }
+  return max;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
